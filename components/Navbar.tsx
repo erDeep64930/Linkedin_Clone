c5b5e439a8566826d9b22f9d91b2e714b401cda0
erDeep640390 <erdeep64930@gmail.com>
2024-06-17 11:58:52 +0530

    navbar has been created and clerk authentication has been added

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,6 +2,14 @@ import Image from "next/image";
 import React from "react";
 import SearchInput from "./SearchInput";
 import NavItems from "./NavItems";
+import {
+  SignedIn,
+  SignedOut,
+  SignIn,
+  SignInButton,
+  UserButton,
+} from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
 
 const Navbar = () => {
   return (
@@ -11,14 +19,26 @@ const Navbar = () => {
         <div className="flex items-center gap-2">
           <Image src="/logo.png" alt="logo" width={35} height={35} />
 
-          <div>
+          <div className="md:block hidden">
             <SearchInput />
           </div>
         </div>
 
         {/* menuitem */}
-        <div className="flex items-center gap-5">
-          <NavItems />
+        <div className=" flex items-center gap-5">
+          <div className="md:block hidden">
+            <NavItems />
+          </div>
+        </div>
+        <div>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+          <SignedOut>
+            <Button className="rounded-full">
+              <SignInButton />
+            </Button>
+          </SignedOut>
         </div>
       </div>
     </div>
