ac50efec3672b72e00978ad081c9b9f88a953c2c
erDeep640390 <erdeep64930@gmail.com>
2024-06-17 09:52:32 +0530

    navbar  has been design

---
+++ components/NavItems.tsx
@@ -0,0 +1,36 @@
+import { Bell, BriefcaseBusiness, HomeIcon, MessageCircleMore, Users } from "lucide-react";
+import Link from "next/link";
+import React from "react";
+
+type NAVITEMS = {
+  src: string,
+  icon: JSX.Element,
+  text: string,
+}
+
+const navItems: NAVITEMS[] = [
+  { src: "/home", icon: <HomeIcon />, text: "Home" },
+  { src: "/networks", icon: <Users />, text: "My Network" },
+  { src: "/job", icon: <BriefcaseBusiness />, text: "Job" },
+  { src: "/message", icon: <MessageCircleMore />, text: "Messaging" },
+  { src: "/notification", icon: <Bell />, text: "Notification" },
+];
+
+const NavItems = () => {
+  return (
+    <div className="flex gap-8">
+      {
+        navItems.map((navItem, index) => {
+          return (
+            <div className="flex flex-col items-center cursor-pointer text-gray-500 hover:text-black" key={index}>
+              <span>{navItem.icon}</span>
+              <Link href={navItem.src}>{navItem.text}</Link>
+            </div>
+          )
+        })
+      }
+    </div>
+  )
+}
+
+export default NavItems;
---
+++ components/SearchInput.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Input } from "@/components/ui/input"
+
+const SearchInput = () => {
+  return (
+    <div>
+<Input type="text" placeholder='Search...' className='bg-slate-400'/>
+    </div>
+  )
+}
+
+export default SearchInput
