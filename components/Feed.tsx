36f5e9a83cf9b9adaa4de8367669a766bcc0ee6e
erDeep640390 <erdeep64930@gmail.com>
2024-06-19 12:48:58 +0530

    border has been removed

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -7,7 +7,7 @@ const Feed = async ({user}:{user:any}) => {
   const userData = JSON.parse(JSON.stringify(user));
   const posts = await getAllPosts();
   return (
-    <div className='border flex-1 border-black'>
+    <div className='flex-1 '>
       <PostInput user={userData}/>
       <Posts posts={posts}/>
     </div>
