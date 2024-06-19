51089c8e702b83aa5c266ef91dda154618b23ee4
erDeep640390 <erdeep64930@gmail.com>
2024-06-19 11:58:26 +0530

    post page has been updated

--- components/Post.tsx
+++ components/Post.tsx
@@ -1,39 +1,51 @@
-"use client";
-import React from "react";
-import { Badge } from "@/components/ui/badge"
-import ProfilePhoto from "./shared/ProfilePhoto";
-import { useUser } from "@clerk/nextjs";
-import { Button } from "./ui/button";
-import { Trash } from "lucide-react";
-import SocialOptions from "./SocialOptions";
-import { IPostDocument } from "@/models/post.model";
-import PostContent from "./PostContent";
+"use client"
+import React from 'react'
+import ProfilePhoto from './shared/ProfilePhoto'
+import { useUser } from '@clerk/nextjs'
+import { Button } from './ui/button'
+import { Trash2 } from 'lucide-react'
+import { Badge } from './ui/badge'
+import { IPostDocument } from '@/models/post.model'
+import PostContent from './PostContent'
+import SocialOptions from './SocialOptions'
+import ReactTimeago from "react-timeago";
 
-const Post = ({post}:{post:IPostDocument}) => {
-  const user = useUser();
-  const fullName = post?.user?.firstName + " " + post?.user?.lastName
-  console.log(user);
 
-  return (
-    <div className="bg-white my-2 rounded-lg border border-gray-300">
-      <div className="flex items-center gap-3 p-4">
-        <ProfilePhoto src={post?.user?.profilePhoto!}/> 
-        <div className="flex items-center justify-between w-full">
-          <div>
-            <h1 className="text-sm font-semibold">{fullName} <Badge variant={"secondary"} className="ml-2">You</Badge> </h1>
-            <p className="text-xs text-gray-500">@{user ? user?.username : "username"}</p>
-            <p className="text-xs text-gray-500">1hr ago</p>
-          </div>
-          <Button size={"icon"} className="rounded-full" variant={"outline"}>
-            <Trash />
-          </Button>
+const Post = ({ post }: { post: IPostDocument }) => {
+    const { user } = useUser();
+    const fullName = post?.user?.firstName + " " + post?.user?.lastName;
+    const loggedInUser = user?.id === post?.user?.userId;
+
+    return (
+        <div className='bg-white my-2 mx-2 md:mx-0 rounded-lg border border-gray-300'>
+            <div className=' flex gap-2 p-4'>
+                <ProfilePhoto src={post?.user?.profilePhoto!} />
+                <div className='flex items-center justify-between w-full'>
+                    <div>
+                        <h1 className='text-sm font-bold'>{fullName} <Badge variant={'secondary'} className='ml-2'>You</Badge></h1>
+                        <p className='text-xs text-gray-500'>@{user ? user?.username : "username"}</p>
+
+                        <p className='text-xs text-gray-500'>
+                            <ReactTimeago date={new Date(post.createdAt)} />
+                        </p>
+                    </div>
+                </div>
+                {/* <div>
+                    {
+                        loggedInUser && (
+                            <Button onClick={() => {
+                                const res = deletePostAction(post._id);
+                            }} size={'icon'} className='rounded-full' variant={'outline'}>
+                                <Trash2 />
+                            </Button>
+                        )
+                    }
+                </div> */}
+            </div>
+            <PostContent post={post} />
+            <SocialOptions />
         </div>
-        <PostContent post={post}/>
-       {/* postcontent */}
-       <SocialOptions />
-      </div>
-    </div>
-  );
-};
+    )
+}
 
-export default Post;
+export default Post
--- components/SocialOptions.tsx
+++ components/SocialOptions.tsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import { Button } from './ui/button'
+import { MessageCircle, Repeat, Send, ThumbsUp } from 'lucide-react'
 
 const SocialOptions = () => {
   return (
-    <div>SocialOptions</div>
+    <div className='flex items-center m-1 justify-center'>
+<Button variant={'ghost'}>
+  <ThumbsUp className='flex items-center gap-2 rounded-lg text-gray-600 hover:text-black'/>
+  <p>Like</p>
+</Button>
+<Button variant={'ghost'}>
+  <MessageCircle className='flex items-center gap-2 rounded-lg text-gray-600 hover:text-black'/>
+  <p>Message</p>
+</Button>
+<Button variant={'ghost'}>
+  <Repeat className='flex items-center gap-2 rounded-lg text-gray-600 hover:text-black'/>
+  <p>Comments</p>
+</Button>
+<Button variant={'ghost'}>
+  <Send className='flex items-center gap-2 rounded-lg text-gray-600 hover:text-black'/>
+  <p>Repost</p>
+</Button>
+    </div>
   )
 }
 
